7ed9bd0541a91e874157b562cb296f61aaaa9f98
Ricardo <ricardovws@gmail.com>
2020-06-01 21:37:54 -0300

    first commit

---
+++ EngInt2/EngInt2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EngInt2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
